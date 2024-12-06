ad379a8914ad13e5f13bccf060d0b3801f406b63
Jaska <pusanteemu@gmail.com>
2024-12-06 11:25:57 +0200

    fix cli build

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,36 +1,11 @@
-use app::App;
 use args::Args;
 use args::Command;
 use pupynet_core::Pupynet;
 use clap::Parser;
 
 mod args;
-mod app;
 
 #[tokio::main]
 async fn main() {
 	let args = Args::parse();
-
-    let pupynet = Pupynet::new();
-	
-	let cmd = match args.cmd {
-		Some(cmd) => cmd,
-		None => {
-			println!("no command specified");
-			return;
-		}
-	};
-
-	match cmd {
-		Command::Gui => {
-			iced::application("Pupynet", App::update, App::view)
-				.subscription(f)
-				.
-				.run_with(App::new)
-				.unwrap();
-		}
-		Command::Copy { src, dest } => {
-			println!("copy {} to {}", src, dest);
-		}
-	}
 }
